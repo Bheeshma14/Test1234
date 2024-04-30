477ff4d91a2e70e7a2e88b21568904ef592727f6
Bheeshma14 <bheeshmadevimedapati@gmail.com>
2024-04-30 18:20:41 +0530

    File added

--- file_1.c
+++ file_1.c
@@ -10,10 +10,10 @@ int main() {
     sum = number1 + number2;
 
     printf("%d + %d = %d", number1, number2, sum);
-    return 0;
+    return 1;
 }
 // function added in branch1
-int func_add(int a, int b)
+int func_add(int b, int a)
 {
         // i am in main branch
         // i am making this changes in
