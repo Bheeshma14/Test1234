4e6caa1e603d0c00432e149fc8e855b32b4ae20f
Bheeshma14 <bheeshmadevimedapati@gmail.com>
2024-04-30 18:31:00 +0530

    file updated

--- file_1.c
+++ file_1.c
@@ -3,7 +3,7 @@ int main() {
 
     int number2, number1, sum;
 
-    printf("Enter two integers: ");
+    printf("Enter two integers values: ");
     scanf("%d %d", &number1, &number2);
 
     // calculate the sum
@@ -15,6 +15,7 @@ int main() {
 // function added in branch1
 int func_add(int b, int a)
 {
+	// Start of the comments
         // i am in main branch
         // i am making this changes in
         // one more change
